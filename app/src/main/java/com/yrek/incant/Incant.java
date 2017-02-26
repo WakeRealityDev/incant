e3030b8bdd97319ea645fd149c76685f08f1d824
Wake Reality (Dev) <development@wakereality.com>
2017-02-26 10:54:44 -0500

    Expand initial story listing to have more technically diverse choices.

--- app/src/main/java/com/yrek/incant/Incant.java
+++ app/src/main/java/com/yrek/incant/Incant.java
@@ -25,7 +25,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
