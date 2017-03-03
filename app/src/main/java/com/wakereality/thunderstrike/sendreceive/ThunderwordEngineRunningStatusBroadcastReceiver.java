eba8d7b42b42d9524cf730984e760829aca8c73a
Wake Reality (Dev) <development@wakereality.com>
2017-03-02 21:30:17 -0500

    ThunderwordEngineRunningStatusBroadcastReceiver gets information about sender app and when it started

--- app/src/main/java/com/wakereality/thunderstrike/sendreceive/ThunderwordEngineRunningStatusBroadcastReceiver.java
+++ app/src/main/java/com/wakereality/thunderstrike/sendreceive/ThunderwordEngineRunningStatusBroadcastReceiver.java
@@ -15,12 +15,17 @@ import org.greenrobot.eventbus.EventBus;
 public class ThunderwordEngineRunningStatusBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        // appStartWhen can tell outside apps if there is a break in continuity of information due to unexpected engine provider restart.
+        long appStartTime = intent.getLongExtra("appStartWhen", 0L);
         int stateCode = intent.getIntExtra("stateCode", -1);
+        String sender = intent.getStringExtra("sender");
         if (stateCode != -1) {
             int index = intent.getIntExtra("index", -1);
             boolean clearIfPresent = intent.getBooleanExtra("clearIfPresent", false);
-            Log.v("EngineRunStatus", "[ThunderClap][shareRemGlk] #" + index + " stateCode " + stateCode + " clearIfPresent " + clearIfPresent);
+            Log.v("EngineRunStatus", "[ThunderClap][RUNNING_STATUS][shareEngineStatus] #" + index + " stateCode " + stateCode + " clearIfPresent " + clearIfPresent + " appStartTime " + appStartTime + " sender " + sender);
             EventBus.getDefault().post(new EventEngineRunningStatus(stateCode));
+        } else {
+            Log.w("EngineRunStatus", "[ThunderClap][RUNNING_STATUS][shareEngineStatus] stateCode -1, skipping internal processing appStartTime " + appStartTime + " sender " + sender);
         }
     }
 }
