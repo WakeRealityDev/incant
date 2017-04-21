927eaaa32a110940a69e8094798fcf78db381d85
Wake Reality (Dev) <development@wakereality.com>
2017-04-21 18:30:56 -0400

    plant idea for csv list of stories

---
+++ storyfinding/src/main/java/com/wakereality/storyfinding/OpenIntFictionDownloadListActivity.java
@@ -0,0 +1,16 @@
+package com.wakereality.storyfinding;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Stephen A. Gutknecht on 4/21/17.
+ * file extensions: .iflistcsv  .iflistjson .iflistxml
+ *   csv of downloads
+ *
+ *   import file, put in special folder public, process as chocie to bring in on the browse list
+ *
+ *   Unique verbose extesions are use to be distinct. can use media store to search and not worry for another purpose/format.
+ */
+
+public class OpenIntFictionDownloadListActivity extends AppCompatActivity {
+}
